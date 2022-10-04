5b9e3951dd848f156617dd5128376747b96f72af
nazeersathak <nazeersathak@gmail.com>
2022-10-04 23:25:13 +0530

    Netflix Clone final

---
+++ src/Nav.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Nav.css";
+
+function Nav() {
+  return (
+    <div className="nav">
+      <h1 className="nav__logo">NETFLIX</h1>
+    </div>
+  );
+}
+
+export default Nav;
